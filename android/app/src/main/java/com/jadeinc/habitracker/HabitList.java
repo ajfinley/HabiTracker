a302244b2886049874f1d0268e51400927f20959
James Hong <jameshong623@gmail.com>
2017-04-25 01:03:58 -0700

    correcting the action bar and adding a button to the right

--- android/app/src/main/java/com/jadeinc/habitracker/HabitList.java
+++ android/app/src/main/java/com/jadeinc/habitracker/HabitList.java
@@ -6,10 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import android.widget.CheckBox;
 import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.graphics.Color;
 
 import java.util.List;
 
@@ -18,16 +24,26 @@ public class HabitList extends AppCompatActivity {
 
     public static final String TAG = "HabitList";
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         makeCallDB();
 
+
         setContentView(R.layout.activity_habit_list);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        myToolbar.setTitleTextColor(Color.WHITE);
+
+
 
         TextView tvDisplayDate0 = (TextView) findViewById(R.id.tvDate0);
         TextView tvDisplayDate1 = (TextView) findViewById(R.id.tvDate1);
         TextView tvDisplayDate2 = (TextView) findViewById(R.id.tvDate2);
+
 
 
         Calendar calendar = Calendar.getInstance();
@@ -64,7 +80,12 @@ public class HabitList extends AppCompatActivity {
     private void generateListTask(List<User> users) {
         User user = users.get(0);
         List<Task> tasks = user.getTasks();
+        List<String> task_names = new ArrayList<>();
+        for (Task task: tasks) {
+            task_names.add(task.getTask());
+        }
     }
+
 
 
 
@@ -79,6 +100,31 @@ public class HabitList extends AppCompatActivity {
                 else
                     break;
                 // TODO: We need to think of a way to populate the list to come up
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.create_task, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
         }
     }
 
